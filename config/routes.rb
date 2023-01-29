ad5d92a310ad0b44fa1b40891113bac70091fc1f
iT2-ej <gpokllmad@gmail.com>
2023-01-29 13:36:34 +0000

    next/guest_login

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,16 @@
 Rails.application.routes.draw do
   get 'homes/top'
+  
+  #root to: 'homes#index'
+  root to: 'homes#top'
+   post '/homes/guest_sign_in', to: 'homes#guest_sign_in'
   devise_for :users , controllers: {
     registrations: 'users/registrations',
-    passwords: 'users/passwords'
+    passwords: 'users/passwords',
   }
   
   devise_scope :user do
-    post "users/guest_sign_in", to: "users/sessions#guest_sign_in"
+    post 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
   end
   
-  root to: 'homes#top'
 end
