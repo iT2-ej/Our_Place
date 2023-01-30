374bd91e6f1ab37716c3848f7196288c887744db
iT2-ej <gpokllmad@gmail.com>
2023-01-30 13:13:38 +0000

    guest_sign_in/completed

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -5,6 +5,12 @@ class HomesController < ApplicationController
   def guest_sign_in
     user = User.find_or_create_by!(email: 'guest@example.com') do |user|
       user.password = SecureRandom.urlsafe_base64
+      user.user_name = "ゲスト"
+      user.last_name = "ゲスト"
+      user.first_name ="ユーザー"
+      user.last_name_kana ="ゲスト"
+      user.first_name_kana ="ユーザー"
+      
       # user.skip_confirmation!  # Confirmable を使用している場合は必要
       # 例えば name を入力必須としているならば， user.name = "ゲスト" なども必要
     end
--- db/migrate/20230126131230_devise_create_users.rb
+++ db/migrate/20230126131230_devise_create_users.rb
@@ -13,6 +13,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.1]
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
+      
 
       ## 名前を保存するカラム
       t.string :user_name, null: false
