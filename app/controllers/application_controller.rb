19c4e44ee85b28d9fb0594323dd4d5b299bc5f00
iT2-ej <gpokllmad@gmail.com>
2023-02-02 12:12:49 +0000

    profile_image

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,6 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:user_name, :last_name, :first_name, :last_name_kana, :first_name_kana])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:user_name, :last_name, :first_name, :last_name_kana, :first_name_kana,:profile_image])
   end
 end
