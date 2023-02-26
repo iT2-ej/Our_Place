d18bac875ab4efe46d306c5678688689f1c8536f
iT2-ej <gpokllmad@gmail.com>
2023-02-26 11:36:47 +0000

    public/layout/completed

--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -38,7 +38,7 @@ class Public::PostsController < ApplicationController
   end
   
   def destroy
-    @post = Post.find_by(params[:id])
+    @post = Post.find(params[:id])
     @post.destroy
     redirect_to public_user_path
   end
